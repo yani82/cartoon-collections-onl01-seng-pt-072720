a02a0e805fbcdfe0e605ac702fa259344cfbaf56
Yani Asari <yani.iman1@gmail.com>
2020-08-07 05:35:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,8 @@ def roll_call_dwarves(list)# code an argument here
   # Your code here
   list = 1 
   while list < 7
-  puts 
+  puts "[1]. #{list}"
+  list += 1
 end
 
 def summon_captain_planet# code an argument here
