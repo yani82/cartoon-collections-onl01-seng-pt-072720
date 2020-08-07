870e6ead15b9ef68b6570594a57cd4f500fb8dcd
Yani Asari <yani.iman1@gmail.com>
2020-08-07 06:21:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def summon_captain_planet(array) # code an argument here
   end
 end
 
-def long_planeteer_calls() # code an argument here
+def long_planeteer_calls(array) # code an argument here
   # Your code here
   array.any? do |words|
     words.size > 4
