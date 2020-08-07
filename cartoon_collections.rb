77696c03dafdc0972a62f78c485c9f8c65e6f93e
Yani Asari <yani.iman1@gmail.com>
2020-08-07 06:11:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-def roll_call_dwarves(dwarves)# code an argument here
+def roll_call_dwarves(dwarves) # code an argument here
   # Your code here
 dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
   end
